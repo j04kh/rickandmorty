35cab050136469424804031f4bd0b17ff94b2798
Joaquin <joaquinhernandezph@gmail.com>
2022-01-22 01:39:43 -0300

    Add: scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import "./App.css";
+import React, { lazy, Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { PerspectiveCamera } from "@react-three/drei";
 
-function App() {
+const App: React.FC = () => {
+  const Scene = lazy(() => import("./components/Scene"));
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="h-screen w-screen bg-gray-900 flex items-center justify-center">
+      <div className="w-full h-full bg-gray-800">
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <Canvas>
+            <PerspectiveCamera
+              fov={70}
+              zoom={1}
+              far={20}
+              position={[1, -2, 5.5]}
+              rotation={[0, 4.5 * (Math.PI / 4), 0]}
+            >
+              <Scene position={[0, 0, 0]} />
+            </PerspectiveCamera>
+            <OrbitControls />
+          </Canvas>
+        </Suspense>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
