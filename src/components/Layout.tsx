ccedf90e114a69547b6db7d8cb1bd8fadd6c9e00
Joaquin <joaquinhernandezph@gmail.com>
2022-01-28 16:18:54 -0300

    Update: fonts

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ import SuspenseFallback from "./SuspenseFallback";
 const Layout: React.FC = ({ children }) => {
   const Scene = lazy(() => import("./Scene"));
   return (
-    <div className="h-screen w-screen bg-gray-900 flex items-center justify-center">
+    <div className="h-screen w-screen bg-gray-900 flex items-center justify-center font-RobotoCondensed">
       <div className="w-full h-full bg-gray-800">
         <Suspense fallback={<SuspenseFallback />}>
           <div className="h-full w-full min-h-screen min-w-screen absolute flex flex-col items-center justify-center z-10">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        RobotoCondensed: ["Roboto Condensed", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
