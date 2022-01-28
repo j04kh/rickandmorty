a615e2680f9a7a5b7142526c35a40aaf6ac1e398
Joaquin <joaquinhernandezph@gmail.com>
2022-01-28 16:08:26 -0300

    Update: home view

--- src/components/Home/Category.tsx
+++ src/components/Home/Category.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 const Category: React.FC<Props> = ({ title, url }) => {
   return (
-    <div className="h-28 w-56 flex flex-col justify-center items-center bg-gray-900 bg-opacity-75 rounded-lg border-2 border-gray-600">
+    <div className="h-20 w-56 flex flex-col justify-center items-center bg-gray-900 bg-opacity-60 rounded-lg border-2 border-cyan-900">
       <h2>{title}</h2>
     </div>
   );
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
 import Wrapper from "../components/Wrapper";
 import Category from "../components/Home/Category";
+import Rick from "../assets/rick.png";
 
 const Home: React.FC = () => {
   return (
-    <Wrapper title="Welcome to Rick's Garage 🧪 ">
+    <Wrapper title="">
+      <div className="flex items-center">
+        <img src={Rick} alt="Rick avatar" className="w-1/2 max-h-48" />
+        <h1 className="text-left text-xl">
+          Hi stranger, <br /> Welcome to my garage!
+        </h1>
+      </div>
       <div className="h-full w-full flex flex-col items-center justify-evenly">
         <Link to="/characters">
           <Category title="CHARACTERS" />
